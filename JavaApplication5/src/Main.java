d186d5ebf57115dadd59fa18cfc96bc4b90702bb
abbasmasood9276 <aabikhan926@gmail.com>
2023-11-16 09:46:25 +0500

    up

--- JavaApplication5/src/Main.java
+++ JavaApplication5/src/Main.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
+// Subject interface
 interface Subject {
     void attach(Observer observer);
     void detach(Observer observer);
     void notifyObservers();
 }
 
-
+// CricketMatch class
 class CricketMatch {
     private String matchDetails;
 
@@ -21,12 +21,12 @@ class CricketMatch {
     }
 }
 
-
+// Observer interface
 interface Observer {
     void update(CricketMatch match);
 }
 
-
+// ConcreteSubject class
 class ConcreteSubject implements Subject {
     private List<Observer> observers = new ArrayList<>();
     private CricketMatch currentMatch;
@@ -54,7 +54,7 @@ class ConcreteSubject implements Subject {
     }
 }
 
-
+// ConcreteObserver class
 class ConcreteObserver implements Observer {
     private CricketMatch currentMatch;
 
@@ -62,48 +62,69 @@ class ConcreteObserver implements Observer {
     public void update(CricketMatch match) {
         this.currentMatch = match;
         // Update the observer's display or perform any other necessary actions
-        System.out.println("Observer updated: New match details received - " + match.getMatchDetails());
+        System.out.println("Observer updated with match details: " + match.getMatchDetails());
     }
 }
 
+// MainScreen class
+class MainScreen implements Observer {
+    private ConcreteSubject subject;
 
-class LiveMatchesScreen implements Observer {
+    MainScreen(ConcreteSubject subject) {
+        this.subject = subject;
+        subject.attach(this);
+    }
+
+    void displayLiveMatches(List<CricketMatch> matches) {
+        // Display the list of live matches
+        System.out.println("Main Screen: Live Matches - " + matches);
+    }
+
+    void selectMatch(CricketMatch match) {
+        // Transition to the ball-by-ball coverage screen
+        BallByBallCoverageScreen coverageScreen = new BallByBallCoverageScreen(subject);
+        subject.setMatch(match); // Simulate selecting a match
+    }
+
     @Override
     public void update(CricketMatch match) {
-        
-        System.out.println("LiveMatchesScreen: Exciting update! A new match is here - " + match.getMatchDetails());
+        // Handle updates from the subject
+        System.out.println("Main Screen: Received match update - " + match.getMatchDetails());
     }
 }
 
+// BallByBallCoverageScreen class
+class BallByBallCoverageScreen implements Observer {
+    private ConcreteSubject subject;
 
-class MatchCoverageScreen implements Observer {
+    BallByBallCoverageScreen(ConcreteSubject subject) {
+        this.subject = subject;
+        subject.attach(this); // Subscribe to updates
+    }
+
     @Override
     public void update(CricketMatch match) {
-       
-        System.out.println("MatchCoverageScreen: Dive into the action! Match details have been updated - " + match.getMatchDetails());
+        // Display ball-by-ball coverage
+        System.out.println("BallByBallCoverageScreen: Displaying ball-by-ball coverage for " + match.getMatchDetails());
     }
 }
 
 public class Main {
     public static void main(String[] args) {
         ConcreteSubject subject = new ConcreteSubject();
+        MainScreen mainScreen = new MainScreen(subject);
 
-        LiveMatchesScreen liveMatchesScreen = new LiveMatchesScreen();
-        MatchCoverageScreen matchCoverageScreen = new MatchCoverageScreen();
+        List<CricketMatch> liveMatches = new ArrayList<>();
+        liveMatches.add(new CricketMatch("Pakistan vs India"));
+        liveMatches.add(new CricketMatch("Australia vs England"));
 
-        subject.attach(liveMatchesScreen);
-        subject.attach(matchCoverageScreen);
+        mainScreen.displayLiveMatches(liveMatches);
 
-        CricketMatch newMatch = new CricketMatch("Match1 Details");
+        // Simulate user clicking on a match
+        mainScreen.selectMatch(liveMatches.get(0));
+
+        // Simulate a new match starting and updating both screens
+        CricketMatch newMatch = new CricketMatch("Australia vs England");
         subject.setMatch(newMatch);
-
-      
-
-        subject.detach(matchCoverageScreen);
-
-        CricketMatch anotherMatch = new CricketMatch("Match2 Details");
-        subject.setMatch(anotherMatch);
-
-        
     }
 }
